c9571caa2b9db884c0c34a60f834b2321282203f
Bifaniii <guilherme.bifanis@gmail.com>
2026-09-14 19:28:33 -0300

    chore: initial commit

---
+++ octopus-msmedications/src/test/java/com/br/octopus_msmedications/OctopusMsmedicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.octopus_msmedications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctopusMsmedicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
