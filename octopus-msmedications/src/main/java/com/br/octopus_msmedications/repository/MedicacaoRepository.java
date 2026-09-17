39f6a658fb69f4329ffe0ae8ce6d1785fd80713d
Bifaniii <guilherme.bifanis@gmail.com>
2026-09-17 18:51:58 -0300

    Corrige problema de build por nome de variáveis incorretas

--- octopus-msmedications/src/main/java/com/br/octopus_msmedications/repository/MedicacaoRepository.java
+++ octopus-msmedications/src/main/java/com/br/octopus_msmedications/repository/MedicacaoRepository.java
@@ -2,10 +2,11 @@ package com.br.octopus_msmedications.repository;
 
 import com.br.octopus_msmedications.domain.Medicacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 import java.util.List;
 
-public interface MedicationRepository extends JpaRepository<Medication, Long> {
+public interface MedicacaoRepository extends JpaRepository<Medicacao, Long> {
     List<Medicacao> findByFabricanteIgnoreCase(String fabricante);
     Optional<Medicacao> findBynomeComercialIgnoreCase(String nomeComercial);
 }
